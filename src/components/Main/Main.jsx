91214330cd521521960e332320363f7cb58467f3
Anıl Çelik <anlclk11@hotmail.com>
2023-10-18 14:10:33 +0300

    Figma projesi kuruldu.

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Main() {
+    return(
+        <main className="container">
+            <h2>Colors</h2>
+        </main>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx}"
+  ],
+  theme: {
+    extend: {
+      height: {
+        '41': '61.06px'
+      } 
+    },
+  },
+  plugins: [],
+}
+
